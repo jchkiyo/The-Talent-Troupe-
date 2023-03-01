c664c402c3a80137dd3ecee0f7aff181dafeddcd
Tan wei herng <weiherng2000@gmail.com>
2023-03-02 01:35:00 +0800

    added more stuff

--- The Talent Troupe/ClientApp/src/components/pages/Bigpurchaseplanner.js
+++ The Talent Troupe/ClientApp/src/components/pages/Bigpurchaseplanner.js
@@ -1,17 +1,23 @@
 import './Bigpurchase.css';
 import React from 'react';
+import hdbpic from "../../assets/hdb.jpg";
+import carpic from "../../assets/car.jpg";
+import { Link } from "react-router-dom";
 
 
 export default function Bigpurchaseplanner() {
   return (
     <>
-      <div className = "hdb">
-      <div class="text-block">
-        <h4>Nature</h4>
-        <p>What a beautiful sunrise</p>
+      <div className = "parent">
+          <Link to = "/hdbplanner">
+            <img  className = "hdb"src={hdbpic} alt = "" height = "700" width = "750"/>
+          </Link >
+          <Link to = "/carplanner">
+            <img  className = "car"src={carpic} alt = "" height = "700" width = "750"/>
+          </Link>
       </div>
-        <button>Click here to caluclate HDB</button>
-      </div>
+      <h1 id = "texthdb">HDB purchase planner</h1>
+      <h1 id = "textcar">Car depreciation calculator</h1>
     </>
   );
 }
