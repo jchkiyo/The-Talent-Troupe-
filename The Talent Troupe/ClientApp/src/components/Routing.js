8fb18b907b78b28f6f2567c11eb47f54c1798ba6
LimJiaEarn <jiae4rn@gmail.com>
2023-03-05 01:32:34 +0800

    Added new UI for ViewHDB and BigPurchase

--- The Talent Troupe/ClientApp/src/components/Routing.js
+++ The Talent Troupe/ClientApp/src/components/Routing.js
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-//import Homepage from "./pages/HomePage";
 import Retirementplanner from "./page/RetirementPlanner";
 import About from "./page/About";
-//import Order from "./pages/Order";
 import Login from "./page/Login";
 import Signup from "./page/Signup";
 import Home from "./page/Home";
+import Viewhdbprices from "./page/Viewhdbprices";
 import Bigpurchaseplanner from "./page/Bigpurchaseplanner";
 
 export default function Routing() {
@@ -15,13 +14,17 @@ export default function Routing() {
 
       <Route path="/retirementplanner" element={<Retirementplanner />} />
 
+      <Route path="/bigpurchaseplanner" element={<Bigpurchaseplanner />} />
+
+      <Route path="/viewhdbprices" element={<Viewhdbprices />} />
+
       <Route path="/about" element={<About />} />
       
       <Route path="/login" element={<Login />} />
 
       <Route path="/signup" element={<Signup />} />
 
-      <Route path="/bigpurchaseplanner" element={<Bigpurchaseplanner />} />
+      
       
     </Routes>
   );
