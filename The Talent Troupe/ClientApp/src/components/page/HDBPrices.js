e72ea139dd4346c7f7becf1c40e9cfaac700a0d0
iceiy0803 <116962757+iceiy0803@users.noreply.github.com>
2023-03-07 03:58:33 +0800

    DataTable

--- The Talent Troupe/ClientApp/src/components/page/HDBPrices.js
+++ The Talent Troupe/ClientApp/src/components/page/HDBPrices.js
@@ -1,10 +1,18 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import Axios from "axios";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import { InputText } from "primereact/inputtext";
+import { FilterMatchMode } from "primereact/api";
 
 export default function HDBPrices() {
   const [hdbPrices, sethdbPrices] = useState([]);
-  //const [column, setColumn] = useState([])
+  const [filters, setFilters] = useState({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  });
 
   useEffect(() => {
     fetchHdbPrice();
@@ -12,7 +20,7 @@ export default function HDBPrices() {
 
   const fetchHdbPrice = () => {
     Axios.get(
-      "https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3"
+      "https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&limit=1000"
     ).then((res) => {
       sethdbPrices(res.data.result.records);
     });
@@ -20,37 +28,54 @@ export default function HDBPrices() {
 
   return (
     <div className="App">
-      <h1>HDB Prices</h1>
-      <tbody>
-        <tr>
-          <th>Month</th>
-          <th>Town</th>
-          <th>Flat type</th>
-          <th>Block</th>
-          <th>Street name</th>
-          <th>Storey Range</th>
-          <th>Floor area sqm</th>
-          <th>Flat model</th>
-          <th>Lease commence date</th>
-          <th>Remaining lease</th>
-          <th>Resale price</th>
-        </tr>
-        {hdbPrices.map((item, i) => (
-          <tr key={i}>
-            <td>{item.month}</td>
-            <td>{item.town}</td>
-            <td>{item.flat_type}</td>
-            <td>{item.block}</td>
-            <td>{item.steet_name}</td>
-            <td>{item.storey_range}</td>
-            <td>{item.floor_area_sqm}</td>
-            <td>{item.flat_model}</td>
-            <td>{item.lease_commence_date}</td>
-            <td>{item.remainin_lease}</td>
-            <td>{item.resale_price}</td>
-          </tr>
-        ))}
-      </tbody>
+      <InputText
+        onInput={(e) =>
+          setFilters({
+            global: {
+              value: e.target.value,
+              matchMode: FilterMatchMode.CONTAINS,
+            },
+          })
+        }
+      />
+
+      <DataTable
+        value={hdbPrices}
+        tableStyle={{ minWidth: "50rem" }}
+        sortMode="multiple"
+        filter={filters}
+      >
+        <Column
+          field="town"
+          header="Town"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="flat_type"
+          header="Flat Type"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="floor_area_sqm"
+          header="Floor Area"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="lease_commence_date"
+          header="Lease Commence Date"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+        <Column
+          field="resale_price"
+          header="Resale Price"
+          sortable
+          style={{ width: "25%" }}
+        ></Column>
+      </DataTable>
     </div>
   );
 }
