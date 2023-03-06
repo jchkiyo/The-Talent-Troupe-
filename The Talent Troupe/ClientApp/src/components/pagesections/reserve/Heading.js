9b8c6a60704e32ec6ce22e7f68246011510e7203
Tan wei herng <weiherng2000@gmail.com>
2023-03-06 23:07:55 +0800

    Revert "reverting"
    This reverts commit cb7f5061d7a2de6cab3f1f311f770d1f02717272.

--- The Talent Troupe/ClientApp/src/components/pagesections/reserve/Heading.js
+++ The Talent Troupe/ClientApp/src/components/pagesections/reserve/Heading.js
@@ -5,7 +5,7 @@ export default function Heading() {
       <img
         className="header-reserve"
         src={HeroImage}
-        alt="Little Lemon Ingredients"
+        alt="Pic4"
       ></img>
       <div className="reserve-header-text">
         <h1>Reserve a table</h1>
