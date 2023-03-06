749c8b87ec317621c873327d3a5ef1fe2fd60f3b
Tan wei herng <weiherng2000@gmail.com>
2023-03-06 23:35:12 +0800

    changes

--- The Talent Troupe/ClientApp/src/index.js
+++ The Talent Troupe/ClientApp/src/index.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Homepage';
-import reportWebVitals from './reportWebVitals';
->>>>>>> grandmaster
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
