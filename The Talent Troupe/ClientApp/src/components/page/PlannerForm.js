4aedb88f63ec8654c7b3fa2e60db5bf4c7c37ba6
LimJiaEarn <jiae4rn@gmail.com>
2023-03-12 21:51:11 +0800

    80% Done with forms

--- The Talent Troupe/ClientApp/src/components/page/PlannerForm.js
+++ The Talent Troupe/ClientApp/src/components/page/PlannerForm.js
@@ -4,144 +4,106 @@ import "./PlannerForm.css";
 
 
 export default function PlannerForm(props) {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
-  const [email, setEmail] = useState("");
-  const [tel, setTel] = useState("");
-  const [people, setPeople] = useState(1);
+  const [PlanName, setPlanName] = useState("");
+  const [PlanPurpose, setPlanPurpose] = useState("");
+  const [AmountToSave, setAmountToSave] = useState("");
+  const [MonthlyContribution, setMonthlyContribution] = useState(1);
   const [date, setDate] = useState("");
-  const [occasion, setOccasion] = useState("");
-  const [preferences, setPreferences] = useState("");
   const [comments, setComments] = useState("");
 
 
-  function handleDateChange(e) {
-    setDate(e.target.value);
 
-    var stringify = e.target.value;
-    const date = new Date(stringify);
 
-    props.updateTimes(date);
 
-  }
 
   return (
-    <form className="reservation-form">
+    <form className="planner-form">
       <div>
-        <label htmlFor="fName">First Name</label> <br></br>
+        <label>Plan Name</label> <br></br>
         <input
           type="text"
-          id="fName"
-          placeholder="First Name"
+          id="PlanName"
+          placeholder=" Car Purchase Plan"
           required
           minLength={2}
           maxLength={50}
-          value={fName}
-          onChange={(e) => setFName(e.target.value)}
+          value={PlanName}
+          onChange={(e) => setPlanName(e.target.value)}
         ></input>
       </div>
 
       <div>
-        <label htmlFor="lName">Last Name</label> <br></br>
-        <input
-          type="text"
-          id="lName"
-          placeholder="Last Name"
-          minLength={2}
-          maxLength={50}
-          value={lName}
-          onChange={(e) => setLName(e.target.value)}
-        ></input>
+        <label>Plan Purpose</label> <br></br>
+        <select
+          id="planpurpose"
+          value={PlanPurpose}
+          onChange={(e) => setPlanPurpose(e.target.value)}
+        >
+          <option>HDB Purchase</option>
+          <option>Car Purchase</option>
+          <option>Others</option>
+        </select>
       </div>
-
+      { PlanPurpose==="HDB Purchase" &&
       <div>
-        <label htmlFor="email">Email</label> <br></br>
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          value={email}
-          required
-          minLength={4}
-          maxLength={200}
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
+      <Link className="action-button" to="/Viewhdbprices">
+        View recent HDB prices here!
+      </Link>
       </div>
-
+      }
       <div>
-        <label htmlFor="phonenum">Phone Number</label> <br></br>
-        <input
-          type="tel"
-          id="phonenum"
-          placeholder="(xxx)-xxx-xxxx"
-          value={tel}
-          required
-          minLength={10}
-          maxLength={25}
-          onChange={(e) => setTel(e.target.value)}
-        ></input>
-      </div>
-
-      <div>
-        <label htmlFor="people">Monthly Income</label> <br></br>
+        <label>Amount to save</label> <br></br>
         <input
           type="number"
-          id="people"
-          placeholder="Number of People"
-          value={people}
+          id="amounttosave"
+          placeholder="100 000"
+          value={AmountToSave}
+          required
+          minLength={1}
+          maxLength={25}
+          onChange={(e) => setAmountToSave(e.target.value)}
+        ></input>
+      </div>
+
+      <div>
+        <label>Monthly Contribution</label> <br></br>
+        <input
+          type="number"
+          id="monthlycontribution"
+          placeholder="5 000"
+          value={MonthlyContribution}
           required
           min={1}
           max={100}
-          onChange={(e) => setPeople(e.target.value)}
+          onChange={(e) => setMonthlyContribution(e.target.value)}
         ></input>
       </div>
 
+
+      
+
       <div>
-        <label htmlFor="date">Select Date</label> <br></br>
+        <label>Date Of Creation</label> <br></br>
         <input
           type="date"
           id="date"
           required
           value={date}
-          onChange={handleDateChange}
+
+          onChange={ (e) => {
+            setDate(e.target.value);
+            props.updateTimes(new Date(e.target.value));
+          }}
+
         ></input>
       </div>
 
       <div>
-        <label htmlFor="occasion">Occasion</label> <br></br>
-        <select
-          id="occasion"
-          value={occasion}
-          onChange={(e) => setOccasion(e.target.value)}
-        >
-          <option>None</option>
-          <option>Birthday</option>
-          <option>Anniversary</option>
-          <option>Engagement</option>
-          <option>Other</option>
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor="preferences">Children</label> <br></br>
-        <select
-          id="preferences"
-          value={preferences}
-          onChange={(e) => setPreferences(e.target.value)}
-        >
-          <option>None</option>
-          <option>Indoors</option>
-          <option>Outdoor (Patio)</option>
-          <option>Outdoor (Sidewalk)</option>
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor="comments">Additional Comments</label> <br></br>
+        <label htmlFor="comments">Notes To Self</label> <br></br>
         <textarea
           id="comments"
           rows={8}
-          cols={50}
+          cols={40}
           placeholder="Additional Comments"
           value={comments}
           onChange={(e) => setComments(e.target.value)}
@@ -150,15 +112,15 @@ export default function PlannerForm(props) {
 
       <div>
         <br></br>
-        <small>
-          <p>
-            Note: You cannot edit your reservation after submission. Please
-            double-check your answer before submitting your reservation request.
-          </p>
-        </small>
-        <Link className="action-button" to="/confirmation">
-          Book Table
+        {
+            PlanName.length>0 &&
+            PlanPurpose.length>0 &&
+            AmountToSave > 0 &&
+            MonthlyContribution > 0 &&
+        <Link className="action-button2" to="/confirmation">
+          SAVE PLAN
         </Link>
+        }
       </div>
     </form>
   );
