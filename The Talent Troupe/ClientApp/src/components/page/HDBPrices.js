60082a24bb94f209c0550e3f9f39b0fc1b9b91d4
iceiy0803 <116962757+iceiy0803@users.noreply.github.com>
2023-03-16 00:00:13 +0800

    test

--- The Talent Troupe/ClientApp/src/components/page/HDBPrices.js
+++ The Talent Troupe/ClientApp/src/components/page/HDBPrices.js
@@ -7,75 +7,108 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import { InputText } from "primereact/inputtext";
 import { FilterMatchMode } from "primereact/api";
+import {
+  MDBBtn,
+  MDBBtnGroup,
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBTable,
+  MDBTableBody,
+  MDBTableHead,
+} from "mdb-react-ui-kit";
 
 export default function HDBPrices() {
   const [hdbPrices, sethdbPrices] = useState([]);
-  const [filters, setFilters] = useState({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-  });
+  const [value, setValue] = useState("");
+  const [hdbList, sethdbList] = useState([]);
 
   useEffect(() => {
     fetchHdbPrice();
-  }, []);
+  }, [hdbList, hdbPrices]);
 
   const fetchHdbPrice = () => {
     Axios.get(
-      "https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&limit=1000"
+      `https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&limit=1000`
     ).then((res) => {
       sethdbPrices(res.data.result.records);
+      sethdbList(res.data.result.records);
     });
   };
+  const handleFilter = () => {
+    value === ""
+      ? sethdbList([...hdbPrices])
+      : sethdbList([
+          ...hdbPrices.filter((item) => item.town.match(value.toUpperCase())),
+        ]);
+  };
 
+  console.log(hdbList);
   return (
-    <div className="App">
-      <InputText
-        onInput={(e) =>
-          setFilters({
-            global: {
-              value: e.target.value,
-              matchMode: FilterMatchMode.CONTAINS,
-            },
-          })
-        }
-      />
-
-      <DataTable
-        value={hdbPrices}
-        tableStyle={{ minWidth: "50rem" }}
-        sortMode="multiple"
-        filter={filters}
+    <MDBContainer>
+      <form
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignContent: "center",
+        }}
+        className="d-flex input-group w-auto"
+        onSubmit={handleFilter}
       >
-        <Column
-          field="town"
-          header="Town"
-          sortable
-          style={{ width: "25%" }}
-        ></Column>
-        <Column
-          field="flat_type"
-          header="Flat Type"
-          sortable
-          style={{ width: "25%" }}
-        ></Column>
-        <Column
-          field="floor_area_sqm"
-          header="Floor Area"
-          sortable
-          style={{ width: "25%" }}
-        ></Column>
-        <Column
-          field="lease_commence_date"
-          header="Lease Commence Date"
-          sortable
-          style={{ width: "25%" }}
-        ></Column>
-        <Column
-          field="resale_price"
-          header="Resale Price"
-          sortable
-          style={{ width: "25%" }}
-        ></Column>
-      </DataTable>
-    </div>
+        <input
+          type="text"
+          classname="form-control"
+          placeholder="Search"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <MDBBtn type="submit" color="dark">
+          Submit
+        </MDBBtn>
+      </form>
+
+      <div style={{ marginTop: "100px" }}>
+        <MDBRow>
+          <MDBCol size="12">
+            <MDBTable>
+              <MDBTableHead dark>
+                <tr>
+                  <th scope="col">Index</th>
+                  <th scope="col">Town</th>
+                  <th scope="col">Flat Type</th>
+                  <th scope="col">Block</th>
+                  <th scope="col">Floor Area Sq</th>
+                  <th scope="col">Lease Commence Date</th>
+                  <th scope="col">Item Resale Price</th>
+                </tr>
+              </MDBTableHead>
+              {hdbList.length === 0 ? (
+                <MDBTableBody className="align-center mb-0">
+                  <tr>
+                    <td colSpan={8} className="text-center mb-0">
+                      No data found
+                    </td>
+                  </tr>
+                </MDBTableBody>
+              ) : (
+                hdbList.map((item, index) => (
+                  <MDBTableBody key={index}>
+                    <tr>
+                      <th scope="row">{index + 1}</th>
+                      <td>{item.town}</td>
+                      <td>{item.flat_type}</td>
+                      <td>{item.block}</td>
+                      <td>{item.floor_area_sqm}</td>
+                      <td>{item.lease_commence_date}</td>
+                      <td>{item.resale_price}</td>
+                    </tr>
+                  </MDBTableBody>
+                ))
+              )}
+            </MDBTable>
+          </MDBCol>
+        </MDBRow>
+      </div>
+    </MDBContainer>
   );
 }
